1b6c8c76c6c2c42bb4342b8a05363d0600b8db57
Pedro Garza <huellas_picota0n@icloud.com>
2025-06-07 21:04:34 -0600

    Add Keymaps for git-conflict plugin

--- lua/palonso/core/keymaps.lua
+++ lua/palonso/core/keymaps.lua
@@ -22,3 +22,12 @@ keymap.set("n", "<C-h>", "<C-w>h", { desc = "Ventana izquierda" })
 keymap.set("n", "<C-l>", "<C-w>l", { desc = "Ventana derecha" })
 keymap.set("n", "<C-j>", "<C-w>j", { desc = "Ventana abajo" })
 keymap.set("n", "<C-k>", "<C-w>k", { desc = "Ventana arriba" })
+
+-- Mapeos para Git Conflict (plugin: akinsho/git-conflict.nvim)
+-- Activos únicamente en buffers con conflictos de git
+keymap.set("n", "co", "<Plug>(git-conflict-ours)", { noremap = true, silent = true, desc = "Elegir nuestra versión (ours)" })
+keymap.set("n", "ct", "<Plug>(git-conflict-theirs)", { noremap = true, silent = true, desc = "Elegir su versión (theirs)" })
+keymap.set("n", "cb", "<Plug>(git-conflict-both)", { noremap = true, silent = true, desc = "Elegir ambas versiones" })
+keymap.set("n", "c0", "<Plug>(git-conflict-none)", { noremap = true, silent = true, desc = "Descartar ambas versiones" })
+keymap.set("n", "]x", "<Plug>(git-conflict-next-conflict)", { noremap = true, silent = true, desc = "Ir al siguiente conflicto" })
+keymap.set("n", "[x", "<Plug>(git-conflict-prev-conflict)", { noremap = true, silent = true, desc = "Ir al conflicto anterior" })
